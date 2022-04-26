3ed5358b41c8809ba805ef4e41e96f2a3389576d
tellebma <maxime.bellet@orange.com>
2022-04-26 10:18:34 +0200

    Fin TP-1

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,4 +1,9 @@
 import json
+import yaml
+from jinja2 import Template, Environment, FileSystemLoader
+
+
+env = Environment(loader=FileSystemLoader("templates"))
 
 
 def load_json_data_from_file(file_path):
@@ -6,33 +11,57 @@ def load_json_data_from_file(file_path):
         with open(file_path) as file:
             data = json.load(file)
     except FileNotFoundError:
-        data = "ERREUR"
         print(f"ERREUR | Le fichier {file_path} n'existe pas.")
-        exit()
+        raise(FileNotFoundError)
     return data
 
 
 def load_yaml_data_from_file(file_path):
-    """
-        A compléter ....
-    """
-    pass
+    try:
+        with open(file_path) as file:
+            data = yaml.safe_load(file)
+    except FileNotFoundError:
+        print(f"ERREUR | Le fichier {file_path} n'existe pas.")
+        raise(FileNotFoundError)
+    return data
 
 
 def render_network_config(template_name, data):
     """
-        A compléter ....
+    Param:
+        tempalte_name
+        data
     """
-    pass
+    # recupère les informations du tempalte.
+    template = env.get_template(template_name)
+    # render le template avec la data.
+    return template.render(data)
+    
 
 
 def save_built_config(file_name, data):
-    """
-        A compléter ....
-    """
-    pass
+    with open(file_name,"w") as file:
+        file.write(data)
+        return True
+    print('Erreur ?')
+    return False
 
 
 if __name__ == "__main__":
-    d = load_json_data_from_file("./data/R2.json")
-    print(d["hostname"])
+    
+    equipments = ["R2","ESW2"]
+    use_json = False
+    use_yaml = True
+    for equipment in equipments:    
+        if use_json:
+            # JSON
+            data = load_json_data_from_file(f"./data/{equipment}.json")
+        elif use_yaml:
+            # YAML
+            data = load_yaml_data_from_file(f"./data/{equipment}.yaml")
+        
+        template = render_network_config(f"./{equipment}.j2",data)
+        
+        save_built_config(f"./config/config_{equipment}.conf",template)
+        print(template)
+    
